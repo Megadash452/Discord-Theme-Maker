d4c0b063dd473dc4286426eb273017201fc10148
Martin Molina <64887017+Megadash452@users.noreply.github.com>
2021-11-07 21:44:03 -0500

    chats button functionality

--- script/ts/main.ts
+++ script/ts/main.ts
@@ -169,6 +169,11 @@ interface Folder {
 }
 
 
+document.querySelector("#notice button")?.addEventListener('click', () => {
+    document.querySelector("#app-base")!.classList.remove("show-notice");
+});
+
+
 
 function getAcronym(str: string): string {
     str = str.trim();
@@ -206,7 +211,7 @@ function appendTemplateNode(template: HTMLElement, element: HTMLElement, templat
     element.appendChild(template.cloneNode(true));
 }
 
-function activeBtnArray(...btns: Array<HTMLElement>) {
+function activeBtns(...btns: Array<HTMLElement>) {
     // buttons will have a relationship where when one is clicked, it gets .active, but buttons with .active have the class removed
     for (let btn of btns)
         btn.addEventListener('click', () => {
@@ -218,9 +223,8 @@ function activeBtnArray(...btns: Array<HTMLElement>) {
             }
         });
 }
-function activeBtnRelation(btnsHolder: HTMLElement) {
-    // take all buttons in a div and do what activeBtnArray does
-    let btns = Array.from(btnsHolder.querySelectorAll("button"));
+function activeBtnArray(btns: Array<HTMLElement>) {
+    // take all buttons in Array and do what activeBtns does
     for (let btn of btns)
         btn.addEventListener('click', () => {
             if (! btn.classList.contains("active")) {
@@ -230,6 +234,10 @@ function activeBtnRelation(btnsHolder: HTMLElement) {
                 btn.classList.add("active");
             }
         });
+}
+function activeBtnRelation(btnsHolder: HTMLElement) {
+    let btns = Array.from(btnsHolder.querySelectorAll("button"));
+    activeBtnArray(btns);
 }
 
 function linkToTabs(btnsHolder: HTMLElement) {
@@ -267,19 +275,6 @@ function setToggleClasses(element: HTMLElement, classes: Array<string>, target=e
 }
 
 
-appendTemplateElement(
-    document.querySelector("#active-now-tmp")!,
-    document.querySelector("#main-content .sidebar > .scroller")!
-);
-    
-activeBtnRelation(document.querySelector("#main-content .head .tab-btns")!);
-linkToTabs(document.querySelector("#main-content .head .tab-btns")!);
-
-
-document.querySelector("#notice button")?.addEventListener('click', () => {
-    document.querySelector("#app-base")!.classList.remove("show-notice");
-});
-
 
 const homeBtn = document.getElementById("home-btn") as HTMLLIElement;
 const guilds = document.getElementById("guilds") as HTMLElement;
@@ -303,17 +298,14 @@ function removeAllActiveGuilds() {
 document.querySelectorAll("#home-btn, #servers li:not(.folder), #guilds .guild-actions").forEach(guild => {
     guild.addEventListener('click', () => {
         setActiveGuild(guild as HTMLLIElement);
-        // TODO: set #app-base .head content
     });
 });
 document.querySelectorAll("#dms-ping li").forEach(guild => {
     guild.addEventListener('click', () => {
         setActiveGuild(guild as HTMLLIElement);
         homeBtn.classList.add("active");
-        // TODO: set #app-base .head content
     });
 });
-
 
 
 
@@ -397,7 +389,6 @@ function writePrivateChats(data: Array<any>) { // Array<Friend | GroupChat>
 
         element.setAttribute('href', dm.href);
         element.querySelector(".title") ! .innerHTML = dm.name;
-        element.querySelectorAll("path[icon-data]")?.forEach(assignIconData);
 
         dms!.appendChild(element);
 
@@ -450,12 +441,15 @@ function writeServers(data: Array<any>) {
             });
         } else { // is server
             element = manageServer(guild).querySelector("li, .item") as HTMLLIElement;
+            applyMaskAndBadge(
+                element,
+                guild.pings, guild.currentEvent
+            );
             element.addEventListener('click', () => {
                 setActiveGuild(element);
                 // TODO: set #app-base .head content
             });
         }
-        element.querySelectorAll("path[icon-data]")?.forEach(assignIconData);
         document.getElementById("servers") ! .appendChild(element);
     });
 
@@ -473,11 +467,6 @@ function writeServers(data: Array<any>) {
         if (server.unreads || server.pings > 0)
             element.querySelector("li, .item") ! .classList.add("unreads");
 
-        applyMaskAndBadge(
-            element.querySelector("li, .item") as HTMLLIElement,
-            server.pings, server.currentEvent
-        );
-
         return element;
     }
 }
@@ -487,6 +476,26 @@ fetch("https://raw.githubusercontent.com/Megadash452/Discord-Theme-Maker/master/
 ).then(json => {
     writePrivateChats(json.privateChats);
     writeServers(json.servers);
+
+    activeBtnArray(
+        Array.from(
+            document.querySelectorAll("#sidebar .channels .dm-channel[href]")!
+        )
+    );
+    // activeBtnArray(
+    //     Array.from(
+    //         document.querySelectorAll("#servers .item")!
+    //     )
+    // );
+
+    document.querySelectorAll("path[icon-data]")?.forEach(assignIconData);
+    document.querySelectorAll("#private-chats .dm-channel, #guilds .item[href]")?.forEach(chat => {
+        chat.addEventListener('click', e => {
+            e.preventDefault()
+            window.history.pushState({}, document.title, chat.getAttribute("href"));
+        });
+        console.log("when click", chat, ", will go to url:", chat.getAttribute("href"));
+    });
 }).catch(error => {
     console.log("error: ", error);
 });
@@ -573,7 +582,6 @@ function assignIconData(path: Element) {
     
     path.setAttribute('d', rtrn);
 }
-document.querySelectorAll("path[icon-data]")?.forEach(assignIconData);
 
 {
     let friendInput = document.getElementById("new-friend-search-bar") as HTMLInputElement;
@@ -644,13 +652,289 @@ document.querySelectorAll("path[icon-data]")?.forEach(assignIconData);
                     friendInput.value.length - 1 - hashIndex() > charsInTag)
                         friendInput.value = friendInput.value.slice(0, -1);
             }
-            submit.disabled = false
+            submit.disabled = false;
             // maybe change the tag numbers in overlay???
         }
         else {
             overlay.innerHTML = friendInput.value + "#0000";
             submit.disabled = false;
         }
-        formNormal()
+        formNormal();
     }); 
+}
+
+
+activeBtnRelation(document.querySelector("#main-content .head .tab-btns")!);
+linkToTabs(document.querySelector("#main-content .head .tab-btns")!);
+// ? sidebar.content="active-now" by default
+appendTemplateElement(
+    document.querySelector("#active-now-tmp")!,
+    document.querySelector("#main-content .sidebar > .scroller")!
+);
+
+// --- When click guild, dm contents, dm chats
+{
+    let mainContent = document.getElementById("main-content") as HTMLElement;
+    let head = mainContent.querySelector(".head") as HTMLElement;
+    let main = mainContent.querySelector("main") as HTMLElement;
+    let sidebar = mainContent.querySelector(".sidebar") as HTMLElement;
+
+    // FRIENDS BUTTON
+    document.getElementById("friends") ! .addEventListener('click', e => {
+        e.preventDefault();
+        if (document.getElementById("friends")!.classList.contains("active"))
+            return;
+
+        // TODO: window.history.pushState({}, document.title, window.location + "/friends");
+        
+        head.querySelector(".info") ! .innerHTML = `
+            <h3 class="header">
+                <svg class="section-icon" x="0" y="0" width="24" height="24" viewbox="0 0 24 24">
+                    <path fill="currentColor" icon-data="friend" transform="translate(2 4)""></path>
+                </svg>
+                Friends
+            </h3>
+            <hr class="vertical">
+            <nav class="tab-btns" target="#main-content .content">
+                <button class="active" target="online">Online</button>
+                <button target="all">All</button>
+                <button target="pending">Pending<span class="badge">1</span></button>
+                <button target="blocked">Blocked</button>
+                <button id="add-friend-btn" target="add-friend">Add Friend</button>
+            </nav>
+        `;
+
+        head.querySelector(".tools") ! .innerHTML = `
+            <button id="new-group-btn">
+                <svg width="24" height="24" viewbox="0 0 24 24">
+                    <path fill="currentColor" icon-data="messageAdd"></path>
+                </svg>
+            </button>
+            <hr class="vertical">
+            <button onclick="showInbox()" data-tooltip="Inbox">
+                <svg width="24" height="24" viewbox="0 0 24 24">
+                    <path fill="currentColor" icon-data="inbox"></path>
+                </svg>
+            </button>
+            <button>
+                <svg width="24" height="24" viewbox="0 0 24 24">
+                    <path fill="currentColor" icon-data="help"></path>
+                </svg>
+            </button>
+        `;
+
+        main.innerHTML = `
+            <div class="tab active" name="online">
+                <div class="scroller">
+                    <h2 class="header">Online — 12</h2>
+                    <li class="item">
+                        <div class="user-info">
+                            <svg class="avatar" width="32" height="32">
+                                <foreignObject width="100%" height="100%" mask="url(#user-status-mask)">
+                                    <img src="res/imgs/Madeline.png">
+                                </foreignObject>
+                                <!-- TODO: user status badge -->
+                            </svg>
+                            <div class="name">
+                                <h3 class="title">
+                                    <span class="username">Friend A</span>
+                                    <span class="tag">#0001</span>
+                                </h3>
+                                <span class="subtitle">Online</span>
+                            </div>
+                        </div>
+                        <div class="actions">
+                            <button data-tooltip="Message" tooltip-direction="up">
+                                <svg width="20" height="20" viewbox="0 0 24 24">
+                                    <path fill="currentColor" icon-data="message"></path>
+                                </svg>
+                            </button>
+                            <button data-tooltip="More" tooltip-direction="up">
+                                <svg width="20" height="20" viewbox="0 0 24 24">
+                                    <path fill="currentColor" icon-data="options"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </li>
+                    <div class="scroller empty">
+                        <div class="wrapper">
+                            <img src="res/imgs/empty-online.svg">
+                            <p class="subtitle">No one's around to play with Wumpus.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab" name="all">
+                <div class="scroller">
+                    <!-- TODO: hardcode friend list -->
+                    <li class="item">
+                        
+                    </li>
+                    
+                    <div class="scroller empty">
+                        <div class="wrapper">
+                            <img src="res/imgs/empty-friends.svg">
+                            <p class="subtitle">No one's around to play with Wumpus.</p>
+                            <button>Add Friend</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab" name="pending">
+                <div class="scroller">
+                    <h2 class="header">Pending — 2</h2>
+                    <li class="item">
+                        <div class="user-info">
+                            <svg class="avatar" width="32" height="32">
+                                <foreignObject width="100%" height="100%" mask="url(#)">
+                                    <img src="res/imgs/Madeline.png">
+                                </foreignObject>
+                            </svg>
+                            <div class="name">
+                                <h3 class="title">
+                                    <span class="username">Friend A</span>
+                                    <span class="tag">#0001</span>
+                                </h3>
+                                <span class="subtitle">Incoming Friend Request</span>
+                            </div>
+                        </div>
+                        <div class="actions">
+                            <button class="accept" data-tooltip="Accept" tooltip-direction="up">
+                                <svg width="20" height="20" viewbox="0 0 24 24">
+                                    <path fill="currentColor" icon-data="checkMark" />
+                                </svg>
+                            </button>
+                            <button class="reject" data-tooltip="Ignore" tooltip-direction="up">
+                                <svg width="20" height="20" viewbox="0 0 24 24">
+                                    <path fill="currentColor" icon-data="x" />
+                                </svg>
+                            </button>
+                        </div>
+                    </li>
+                    <li class="item">
+                        <div class="user-info">
+                            <svg class="avatar" width="32" height="32">
+                                <foreignObject width="100%" height="100%" mask="url(#)">
+                                    <img src="res/imgs/Madeline.png">
+                                </foreignObject>
+                            </svg>
+                            <div class="name">
+                                <h3 class="title">
+                                    <span class="username">Friend B</span>
+                                    <span class="tag">#0001</span>
+                                </h3>
+                                <span class="subtitle">Outgoing Friend Request</span>
+                            </div>
+                        </div>
+                        <div class="actions">
+                            <button class="reject" data-tooltip="Cancel" tooltip-direction="up">
+                                <svg width="20" height="20" viewbox="0 0 24 24">
+                                    <path fill="currentColor"icon-data="x" />
+                                </svg>
+                            </button>
+                        </div>
+                    </li>
+                    <div class="scroller empty">
+                        <div class="wrapper">
+                            <img src="res/imgs/empty-pending.svg">
+                            <p class="subtitle">There are no pending friend requests. Here's Wumpus for now.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab" name="blocked">
+                <div class="scroller">
+                    <h2 class="header">Blocked — 1</h2>
+                    <li class="item">
+                        <div class="user-info">
+                            <svg class="avatar" width="32" height="32">
+                                <foreignObject width="100%" height="100%" mask="url(#)">
+                                    <img src="res/imgs/Madeline.png">
+                                </foreignObject>
+                            </svg>
+                            <div class="name">
+                                <h3 class="title">
+                                    <span class="username">Friend B</span>
+                                    <span class="tag">#0001</span>
+                                </h3>
+                                <span class="subtitle">Blocked</span>
+                            </div>
+                        </div>
+                        <div class="actions">
+                            <button class="reject" data-tooltip="Unblock" tooltip-direction="up">
+                                <svg width="20" height="20" viewbox="0 0 24 24">
+                                    <path fill="currentColor" icon-data="unblock" />
+                                </svg>
+                            </button>
+                        </div>
+                    </li>
+                    <div class="scroller empty">
+                        <div class="wrapper">
+                            <img src="res/imgs/empty-blocked.svg">
+                            <p class="subtitle">You can't unblock the Wumpus.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab" name="add-friend">
+                <header>
+                    <h2 class="title">Add Friend</h2>
+                    <p class="subtitle">You can add a friend with their Discord Tag. It's cAsE sEnSitIvE!</p>
+                    <form autocomplete="off" class="wrapper"> <!-- :focus-within, class: success -->
+                        <input id="new-friend-search-bar" type="text" placeholder="Enter a Username#0000" maxlength="37">
+                        <div class="input-overlay"></div>
+                        <button type="submit" disabled>Send Friend Request</button>
+                    </form>
+                    <p class="subtitle message error hidden"></p> <!-- class: hidden, error, success -->
+                </header>
+                <div class="scroller empty">
+                    <div class="wrapper">
+                        <img src="res/imgs/empty-friends.svg">
+                        <p class="subtitle">Wumpus is waiting on friends. You don’t have to though!</p>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        activeBtnRelation(head.querySelector(".tab-btns")!);
+        linkToTabs(head.querySelector(".tab-btns")!);
+
+        sidebar.setAttribute("content", "active-now");
+        sidebar.classList.remove("hidden");
+
+        sidebar.querySelector(".scroller") ! .innerHTML = "";
+        appendTemplateElement(
+            document.querySelector("#active-now-tmp")!,
+            sidebar.querySelector(".scroller")!
+        );
+
+        mainContent.querySelectorAll("path[icon-data]")?.forEach(assignIconData);
+    });
+
+    // STAGE DISCOVERY BUTTON
+    // TODO:
+
+    // NITRO BUTTON
+    document.getElementById("nitro") ! .addEventListener('click', e => {
+        e.preventDefault();
+        if (document.getElementById("nitro")!.classList.contains("active"))
+            return;
+
+        head.querySelector(".info") ! .innerHTML = `
+            <h3 class="header">
+                <svg class="section-icon" x="0" y="0" width="24" height="24" viewbox="0 0 24 24">
+                    <path fill="currentColor" icon-data="nitro""></path>
+                </svg>
+                Nitro
+            </h3>
+        `;
+        
+        head.querySelector(".tools") ! .innerHTML = "";
+
+        main.innerHTML = ""; // TODO:
+
+        sidebar.classList.add("hidden");
+
+        mainContent.querySelectorAll("path[icon-data]")?.forEach(assignIconData);
+    });
 }
