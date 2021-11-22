abedf68534d83986b10d4b29d2fb06d645aff518
Martin Molina <64887017+Megadash452@users.noreply.github.com>
2021-11-22 14:34:25 -0500

    getParams updated

--- script/js/url.js
+++ script/js/url.js
@@ -1,40 +1,37 @@
 "use strict";
 var _a, _b, _c, _d;
-console.log("url.ts, file does not defer");
-console.log(window.location);
-console.log("");
+console.log("url.ts, script does not defer");
 function getUrlParams() {
     let url = window.location.href;
     let paramsStart = url.indexOf('?') + 1;
-    console.log(url[paramsStart]);
     let params = {};
-    let nameBuf = "";
-    let val = "";
-    let eqIndex = 0;
+    let pair = {
+        nameBuf: "",
+        val: ""
+    };
     let storeIn = "nameBuf";
     for (let i = paramsStart; i < url.length; i++) {
         let c = url[i];
-        if (url[i] == '=') {
+        if (url[i] == '=')
             storeIn = "val";
-        }
         else if (url[i] == '&') {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            params[nameBuf] = val; // @ts-ignore
+            params[pair.nameBuf] = pair.val;
+            pair.nameBuf = "";
+            pair.val = "";
             storeIn = "nameBuf";
         }
         else if (storeIn == "nameBuf")
-            nameBuf += url[i];
+            pair.nameBuf += url[i];
         else if (storeIn == "val")
-            val += url[i];
+            pair.val += url[i];
     }
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    params[nameBuf] = val; // @ts-ignore
+    params[pair.nameBuf] = pair.val;
     return params;
 }
 var urlParams = getUrlParams();
 // document.querySelector("base")?.setAttribute('href', window.location.origin);
-(_a = document.querySelector('meta[name="theme-id"]')) === null || _a === void 0 ? void 0 : _a.setAttribute('href', ""); // TODO
-(_b = document.querySelector('meta[name="page"]')) === null || _b === void 0 ? void 0 : _b.setAttribute('href', ""); // TODO
-(_c = document.querySelector('meta[name="section"]')) === null || _c === void 0 ? void 0 : _c.setAttribute('href', ""); // TODO
-(_d = document.querySelector('meta[name="content"]')) === null || _d === void 0 ? void 0 : _d.setAttribute('href', ""); // TODO
+(_a = document.querySelector('meta[name="theme-id"]')) === null || _a === void 0 ? void 0 : _a.setAttribute('content', urlParams.id);
+(_b = document.querySelector('meta[name="page"]')) === null || _b === void 0 ? void 0 : _b.setAttribute('content', urlParams.page);
+(_c = document.querySelector('meta[name="section"]')) === null || _c === void 0 ? void 0 : _c.setAttribute('content', urlParams.sec);
+(_d = document.querySelector('meta[name="content"]')) === null || _d === void 0 ? void 0 : _d.setAttribute('content', urlParams.content);
 //# sourceMappingURL=url.js.map
--- script/ts/url.ts
+++ script/ts/url.ts
@@ -1,43 +1,40 @@
-console.log("url.ts, file does not defer");
-console.log(window.location);
-
-
-console.log("")
+console.log("url.ts, script does not defer");
 
 function getUrlParams() {
     let url = window.location.href;
     let paramsStart = url.indexOf('?') + 1;
-    console.log(url[paramsStart]);
-    let params = {};
+    let params: {[index: string]: string} = {};
 
-    let nameBuf = "";
-    let val = "";
-    let eqIndex = 0;
+    let pair = {
+        nameBuf: "",
+        val: ""
+    };
     let storeIn = "nameBuf";
 
     for (let i = paramsStart; i< url.length; i++) {
         let c = url[i];
 
-        if (url[i] == '=') {
+        if (url[i] == '=')
             storeIn = "val";
-        } else if (url[i] == '&') {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            params[nameBuf] = val; // @ts-ignore
+        else if (url[i] == '&') {
+            params[pair.nameBuf] = pair.val;
+            pair.nameBuf = "";
+            pair.val = "";
             storeIn = "nameBuf";
         } else
             if (storeIn == "nameBuf")
-                nameBuf += url[i];
+                pair.nameBuf += url[i];
             else if (storeIn == "val")
-                val += url[i];
+                pair.val += url[i];
     }
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    params[nameBuf] = val; // @ts-ignore
+    
+    params[pair.nameBuf] = pair.val;
     return params;
 }
 var urlParams = getUrlParams();
 
 // document.querySelector("base")?.setAttribute('href', window.location.origin);
-document.querySelector('meta[name="theme-id"]')?.setAttribute('href', ""); // TODO
-document.querySelector('meta[name="page"]')?.setAttribute('href', ""); // TODO
-document.querySelector('meta[name="section"]')?.setAttribute('href', ""); // TODO
-document.querySelector('meta[name="content"]')?.setAttribute('href', ""); // TODO
+document.querySelector('meta[name="theme-id"]')?.setAttribute('content', urlParams.id);
+document.querySelector('meta[name="page"]')?.setAttribute('content', urlParams.page);
+document.querySelector('meta[name="section"]')?.setAttribute('content', urlParams.sec);
+document.querySelector('meta[name="content"]')?.setAttribute('content', urlParams.content);
