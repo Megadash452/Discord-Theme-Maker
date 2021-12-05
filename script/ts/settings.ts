f508011e4781d9b8e7565dc85105e2f4ebad2885
Martin Molina <64887017+Megadash452@users.noreply.github.com>
2021-12-05 02:29:02 -0500

    more settings

--- script/ts/settings.ts
+++ script/ts/settings.ts
@@ -43,18 +43,88 @@ document.querySelectorAll<HTMLTemplateElement>("template[setting-name]")?.forEac
     if (!sections[section])
         sections[section] = [];
 
-    sections[template.getAttribute('section')!].push(listItem);
+    if (section == "BetterDiscord")
+        listItem.classList.add("bd-item");
+
+    sections[section].push(listItem);
 });
 
+// render sections and buttons
 for (let section in sections) {
-    const heading = document.createElement('h2') as HTMLHeadingElement;
-    heading.classList.add('section');
-    heading.innerText = section;
-    settingsList.appendChild(heading);
+    // section header
+    
+    // only push section header if string not empty
+    if (section) {
+        let heading;
 
+        if (section == "BetterDiscord") {
+            heading = document.createElement('div');
+            heading.classList.add("bd-heading")
+            heading.innerHTML = `
+                <h2 class="section">${section}</h2>
+                <button id="bd-changelog-btn" onclick="openModal('bd-changelog')">
+                    <svg width="16" height="16" viewBox="0 0 24 24">
+                        <path fill="currentColor" icon-data="history"/>
+                    </svg>
+                </button>
+            `;
+        } else {
+            heading = document.createElement('h2');
+            heading.classList.add('section');
+            heading.innerText = section;
+        }
+        settingsList.appendChild(heading);
+    }
+
+    // render buttons in this section
     for (let button in sections[section]) {
         settingsList.appendChild(sections[section][button]);
     }
+
+    // section divider
+    const divider = document.createElement("hr");
+    divider.classList.add("divider")
+    settingsList.appendChild(divider);
+}
+{
+    // Extra stuff that goes after sections (changelog, version, etc)
+    settingsList.insertAdjacentHTML("beforeend", `
+        <button class="item" onclick="openModal('changelog')">Change Log</button>
+        <button class="item">HypeSquad</button>
+        <hr class="divider">
+        <button class="item" id="logout-btn">Log Out</button>
+        <hr class="divider">
+
+        <div class="social">
+            <a href="" title="Twitter">
+                <svg width="20" height="16" viewBox="0 0 20 16">
+                    <path fill="currentColor" icon-data="twitter"/>
+                </svg>
+            </a>
+            <a href="" title="Facebook">
+                <svg width="16" height="16" viewBox="0 0 16 16">
+                    <path fill="currentColor" icon-data="facebook"/>
+                </svg>
+            </a>
+            <a href="" title="Instagram">
+                <svg width="16" height="16" viewBox="0 0 16 16">
+                    <path fill="currentColor" icon-data="instagram"/>
+                </svg>
+            </a>
+            <a href="" title="BetterDiscord">
+                <svg width="16" height="16" viewBox="0 0 2000 2000">
+                    <path fill="currentColor" icon-data="betterDiscord.0"/>
+                    <path fill="currentColor" icon-data="betterDiscord.1"/>
+                </svg>
+            </a>
+        </div>
+
+        <div class="info">
+            <span>Discord Version</span>
+            <span>Operating System</span>
+        </div>
+    `);
+    settingsList.querySelectorAll<SVGPathElement>("path[icon-data]").forEach(assignIconData);
 }
 activeBtnRelation(settingsList);
 
